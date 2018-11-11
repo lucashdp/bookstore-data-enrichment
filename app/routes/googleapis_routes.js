4b5ef3d1339dd06c00d540339f6dd64b2309a928
Lucas Duarte <lucashdp@outlook.com>
2018-11-11 18:51:59 -0200

    feat: Implements get book by id

--- app/routes/googleapis_routes.js
+++ app/routes/googleapis_routes.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     const books = require('google-books-search');
-
+    //GET
     app.get('/v1/public/books', (req, res) => {
         let search = 'a';
 
@@ -8,6 +8,20 @@ module.exports = function (app) {
             search = req.query.search;
 
         books.search(search, function (error, results) {
+            if (!error) {
+                res.send(results);
+            } else {
+                res.send(error);
+            }
+        });
+    });
+    app.get('/v1/public/book/:bookId', (req, res) => {
+        let bookId = 0;
+
+        if (req.params.bookId)
+            bookId = req.params.bookId;
+
+        books.lookup(bookId, function (error, results) {
             if (!error) {
                 res.send(results);
             } else {
